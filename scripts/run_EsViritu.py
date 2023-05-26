765f8fedef5e21f9ddb6dea6843f4a9462d8c8d4
Mike Tisza <michael.tisza@gmail.com>
2023-05-26 16:09:04 -0500

    Update run_EsViritu.py

--- scripts/run_EsViritu.py
+++ scripts/run_EsViritu.py
@@ -35,7 +35,7 @@ required_args.add_argument("-o", "--output_dir",
                            dest="OUTPUT_DIR", type=str, required=True, 
                            help='Output directory name. Will be created if it does not exist. Can be shared with other samples. No space characters, please. ')
 
-
+ES_version='v0.1.2'
 
 optional_args = parser.add_argument_group(' OPTIONAL ARGUMENTS for EsViritu.')
 
@@ -130,4 +130,4 @@ subprocess.call(['bash', str(esviritu_script_path) + '/EsViritu_general.sh',
             str(READS), str(args.SAMPLE), str(args.CPU), str(args.OUTPUT_DIR), 
             str(args.QUAL), str(args.FILTER_SEQS), str(args.COMPARE), 
             str(args.TEMP_DIR), str(args.KEEP), str(args.READ_FMT).lower(), 
-            str(args.version), str(esviritu_script_path)])
+            str(ES_version), str(esviritu_script_path)])
